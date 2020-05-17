4563ae9463f28d1c2caeb6582fc6901de9e4bf1a
Iban <rodorro@hotmail.com>
2020-05-17 12:09:03 +0200

    Update ssh

--- terraform/deployment/ssh/deploy.sh
+++ terraform/deployment/ssh/deploy.sh
@@ -4,5 +4,5 @@ HOST="34.241.218.137"
 USER="ec2-user"
 KEY="../../rodorro.pem"
 
-SSH -i $KEY $USER@HOST \
-    "sudo yum install docker -y && sudo service docker start && sudo docker run -d - p 80:8888 rodorro/myapp"
+ssh -i $KEY $USER@$HOST \
+    "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8888 rodorro/myapp"
